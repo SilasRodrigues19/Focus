4613b86592fb943f9a5baca9795dece33160c46a
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-07-13 12:28:41 -0300

    feat: add aditional colors and font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'gray-darker': '#1A202C',
+      },
+    },
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+    },
+  },
+  plugins: [],
+};
