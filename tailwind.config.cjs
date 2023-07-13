cb5df2b5a559706554a5dffb73d900f90fb4da4b
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-07-13 14:36:05 -0300

    chore: add theme extend config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      backgroundImage: {
+        'clock-bg': "url('/coffee.svg')",
+      },
       colors: {
         'gray-darker': '#1A202C',
       },
